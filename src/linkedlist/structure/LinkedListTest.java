1633cde8354e3559473d2cdca7e48d91e713bfbe
kimjongheon <mje807@gmail.com>
2018-08-22 23:10:13 +0900

    linkedList method -ing

--- src/linkedlist/structure/LinkedListTest.java
+++ src/linkedlist/structure/LinkedListTest.java
@@ -1,5 +1,87 @@
 package linkedlist.structure;
 
 public class LinkedListTest {
-	// 기능 테스트 
+	// 기능 테스트
+	public static void main(String args[]) {
+
+		MyLinkedList linkedList = new MyLinkedListLogic();
+
+		linkedList.add("Test1");
+		linkedList.add("Test2");
+		linkedList.add("Test3");
+		linkedList.add("Test4");
+		linkedList.add("Test5");
+
+		System.out.println("[기본값 add(String element) 후]");
+		System.out.print("현재 linkedList 에 저장된 값은 :");
+		for (int i = 0; i < linkedList.size(); i++) {
+			System.out.print(linkedList.get(i) + ",");
+		}
+		System.out.println("이고 size 는 (" + linkedList.size() + ") 입니다.");
+		System.out.println("============================================================");
+		
+		System.out.println("linkedList.contain(Test3) 결과값 :" + linkedList.contains("Test3"));
+		System.out.println("linkedList.contain(Test9) 결과값 :" + linkedList.contains("Test9"));
+		System.out.println();
+		
+		linkedList.add(2, "AddTest1");
+		System.out.println("[add(2, AddTest1) 메소드 후]");
+		System.out.print("현재 linkedList 에 저장된 값은 :");
+		for (int i = 0; i < linkedList.size(); i++) {
+			System.out.print(linkedList.get(i) + ",");
+		}
+		System.out.println("이고 size 는 (" + linkedList.size() + ") 입니다.");
+		System.out.println("============================================================");
+		
+		linkedList.remove(4);
+		System.out.println("[remove(4) 메소드 후]");
+		System.out.print("현재 linkedList 에 저장된 값은 :");
+		for (int i = 0; i < linkedList.size(); i++) {
+			System.out.print(linkedList.get(i) + ",");
+		}
+		System.out.println("이고 size 는 (" + linkedList.size() + ") 입니다.");
+		System.out.println("============================================================");
+		
+		linkedList.remove("AddTest1");
+		System.out.println("[remove(4) 메소드 후]");
+		System.out.print("현재 linkedList 에 저장된 값은 :");
+		for (int i = 0; i < linkedList.size(); i++) {
+			System.out.print(linkedList.get(i) + ",");
+		}
+		System.out.println("이고 size 는 (" + linkedList.size() + ") 입니다.");
+		System.out.println("============================================================");
+		
+		System.out.println("[toArray() 메소드 결과값]");
+		for (int i = 0; i < linkedList.toArray().length; i++) {
+			System.out.print("linkedList.toArray[" + i + "]:" + linkedList.toArray()[i] + "  ");
+		}
+		System.out.println("\n============================================================");
+		
+		// 새로운 linkedList 추가
+		MyLinkedList addLinkedList = new MyLinkedListLogic();
+
+		addLinkedList.add("Add1");
+		addLinkedList.add("Add2");
+		addLinkedList.add("Add3");
+		
+		linkedList.addAll(addLinkedList);		
+		System.out.println();
+		System.out.println("[addAll(addLinkedList) 메소드 결과값]");	
+		System.out.print("현재 linkedList 에 저장된 값은 :");
+		for (int i = 0; i < linkedList.size()-3; i++) {
+			System.out.print(linkedList.get(i) + ",");
+		}
+		System.out.println("이고 size 는 (" + linkedList.size() + ") 입니다.");
+		System.out.println("============================================================");
+		
+		linkedList.clear();
+		System.out.println("[clear() 메소드 결과값]");
+		System.out.print("현재 linkedList 에 저장된 값은 :");
+		for (int i = 0; i < linkedList.size(); i++) {
+			System.out.print(linkedList.get(i) + ",");
+		}
+		System.out.println("이고 size 는 (" + linkedList.size() + ") 입니다.");
+		System.out.println("============================================================");
+
+	}
 }
--- src/linkedlist/structure/MyLinkedListLogic.java
+++ src/linkedlist/structure/MyLinkedListLogic.java
@@ -1,18 +1,16 @@
 package linkedlist.structure;
 
-
-public class MyLinkedListLogic implements MyLinkedList{
+public class MyLinkedListLogic implements MyLinkedList {
 	//
 	private int size;
-	private String[] array;
 	private Node head;
 	private Node tail;
-	
+
 	public MyLinkedListLogic() {
 		//
 		size = 0;
 	}
-	
+
 	@Override
 	public int size() {
 		//
@@ -21,13 +19,20 @@ public class MyLinkedListLogic implements MyLinkedList{
 
 	@Override
 	public boolean empty() {
-		// 비어있는지 검사
-		return false;
+		//
+		return size == 0;
 	}
 
 	@Override
 	public boolean contains(String element) {
-		// element가 포함되어있으면 true 그렇지 않으면 false 반환
+		// element가 포함되어있으면 true 그렇지 않으면 false 반환	
+		Node findNode = head;
+		for(int i = 0; i<size ; i++) {
+			if(findNode.data.equals(element)) {
+				return true;
+			}
+			findNode = findNode.nextNode;
+		}		
 		return false;
 	}
 
@@ -36,13 +41,13 @@ public class MyLinkedListLogic implements MyLinkedList{
 		//
 		return null;
 	}
-	
+
 	@Override
 	public void add(String element) {
 		// element를 linkedList 맨 뒤에 삽입
 		Node newNode = new Node(element);
-		
-		if(size == 0) {
+
+		if (size == 0) {
 			head = newNode;
 			tail = head;
 		} else {
@@ -50,54 +55,95 @@ public class MyLinkedListLogic implements MyLinkedList{
 			newNode.preNode = tail;
 			tail = newNode;
 		}
-		
+		size++;
 	}
 
 	@Override
 	public void add(int index, String element) {
 		// LinkedList 의 index 에 element값 삽입
-		
+		Node newNode = new Node(element);
+
+		Node findNode = nodeOfListIndex(index);
+
+		pushOfLink(findNode.preNode, newNode, findNode);
+
+		size++;
 	}
 
 	@Override
 	public String get(int index) {
 		// index위치의 값 반환
-		return null;
+		return nodeOfListIndex(index).data;
 	}
 
 	@Override
 	public void remove(String element) {
 		// LinkedList의 element 값 삭제 뒤의 list를 당겨옴
+		Node findNode = findNodeToElement(element);
+
+		pullOfLink(findNode.preNode, findNode, findNode.nextNode);
+
+		clearInNode(findNode);
 		
+		size--;
 	}
 
 	@Override
 	public void remove(int index) {
 		// index위치의 값 삭제 뒤의 list를 당겨옴
+		Node findNode = nodeOfListIndex(index);
+
+		pullOfLink(findNode.preNode, findNode, findNode.nextNode);
 		
+		clearInNode(findNode);
+
+		size--;
 	}
 
 	@Override
 	public void addAll(MyLinkedList sourceList) {
 		// sourceList 값을 LinkedList의 맨뒤부터 삽입
-		
+		String[] array = new String[sourceList.size()];
+		array = sourceList.toArray();
+		for(int i=0; i<(sourceList.size()); i++) {		
+			this.add(array[i]);		
+		}			
 	}
 
 	@Override
 	public void clear() {
 		// 클.리.어
+		for(int i = 0; i<size; i++) {
+			head.data = null;
+			head.preNode = null;
+			
+			head = head.nextNode;			
+		}
+		
+		head = tail;
+		
+		size = 0;
 		
 	}
 
 	@Override
 	public String[] toArray() {
 		// 배열형태로 반환
-		return null;
+		String[] array = new String[size];
+		
+		Node findNode = head;
+		for(int i = 0; i<size ; i++) {
+			
+			array[i] = findNode.data;
+			
+			findNode = findNode.nextNode;
+		}		
+		return array;
 	}
-	
+
 	public class MyStringIterator implements StringIterator {
 		//
-		
+
 		@Override
 		public String next() {
 			// TODO Auto-generated method stub
@@ -113,40 +159,67 @@ public class MyLinkedListLogic implements MyLinkedList{
 		@Override
 		public void remove() {
 			// TODO Auto-generated method stub
-			
+
 		}
 	}
-	
-	private class Node{
+
+	private class Node {
 		// LinkedList에서 사용할 노드
-		
 		private String data;
 		private Node preNode;
 		private Node nextNode;
-		
+
 		public Node(String value) {
 			//
 			this.data = value;
 			this.preNode = null;
 			this.nextNode = null;
 		}
-		
 	}
-	
-	
-	//private Method
-	private String dataOfListIndex(int index) {
+
+	// private Method
+	private Node nodeOfListIndex(int index) {
 		// index위치의 데이터 반환 (linked List의 index가 없어서 index위치를 찾아서 데이터 반환하는 함수)
-		//Node findNode = 
-		return null;
+		Node findNode = head;
+
+		for (int i = 0; i < index; i++) {
+			findNode = findNode.nextNode;
+		}
+		return findNode;
+	}
+
+	private void pushOfLink(Node preNode, Node newNode, Node nextNode) {
+		// 새로운 노드 (newNode) 를 지정된 위치의 노드(nextNode) 와 그 앞 노드(preNode) 사이에 넣고 연결하는 메소드
+		newNode.preNode = preNode;
+		newNode.nextNode = nextNode;
+		preNode.nextNode = newNode;
+		nextNode.preNode = newNode;
+	}
+
+	private void pullOfLink(Node preNode, Node findNode, Node nextNode) {
+		// 지정된 위치의 노드(findNode) 의 앞 노드(preNode)와 뒤 노드(NextNode)를 연결하는 메소드
+		preNode.nextNode = nextNode;
+		nextNode.preNode = preNode;
+	}
+
+	private Node findNodeToElement(String element) {
+		//
+		Node findNode = head;
+
+		while (true) {
+			if (findNode.data == element) {
+				break;
+			}
+			
+			findNode = findNode.nextNode;
+		}
+		return findNode;
 	}
 	
-	private void pushLinkedList() {
-		// 삽입할 위치의 노드를 밀어 새 노드, preNode, nextNode 와 연결하는 함수 -> 미는 함수로 독립
+	private void clearInNode(Node node) {
+		// 노드의 내용을 null로 초기화
+		node.data = null;
+		node.preNode = null;
+		node.nextNode = null;
 	}
-	
-	private void pullLinkedList() {
-		// 삭제한 위치의 노드의 nextNode와 preNode를 당겨 연결하는 함수 -> 당기는 함수로 독립
-	}
-	// 연결하는 함수 만들어야함
 }
